7136a8bb7d87854acb64d58aaff269b6f9b101a5
Rafael Rabelo da Silva <rafaelrabelodasilva@outlook.com>
2022-11-05 19:06:33 -0300

    adf

---
+++ README.md
@@ -0,0 +1,17 @@
+## O Projeto
+
+Projeto criado com intuíto de exercitar a automação que venho aprendendo.<br>
+Infelizmente a única tela que consegui automatizar do site Submarino foi a tela de registro visto que as outras telas são bloqueadas para uso dos cypress. 😢<br>
+Na pasta fixtures existe um arquivo chamado massa-de-dados o qual será usado para gerar os dados para cadastro do usuário. Futuramente irei adicionar um script para que ele gere as informações automaticamente.
+
+## Dependências
+
+Neste projeto foi utilizado somente o **Cypress** o qual irá ser instalado após rodar o comando abaixo:<br>
+
+``npm install``
+
+## Observações
+
+Para abrir o cypress basta digitar o comando abaixo no terminal:<br>
+
+``npx cypress open``
---
+++ cypress/e2e/cadastro.cy.js
@@ -0,0 +1,138 @@
+describe('Tela de cadastro Submarino', () => {
+
+  beforeEach(() => {
+    cy.visit('https://cliente.submarino.com.br/minha-conta/cadastro')
+  })
+
+  it('Verifica mensagens de erro', () => {
+    cy.get('input[name="name"]')
+    .click()
+
+    cy.get('input[name="birthDate"]')
+    .click()
+
+    cy.get('input[name="cpf"]')
+    .click()
+
+    cy.get('input[name="phone"]')
+    .click()
+
+    cy.get('input[name="email"]')
+    .click()
+
+    cy.get('input[name="password"]')
+    .click()
+
+    cy.get('[data-cy="sign-submit"]')
+    .click()
+
+
+    cy.contains('span', 'Por favor, informe o nome e o sobrenome.')
+    .should('be.visible')
+
+    cy.contains('span', 'É necessário informar a data de nascimento.')
+    .should('be.visible')
+
+    cy.contains('span', 'É necessário informar um CPF.')
+    .should('be.visible')
+
+    cy.contains('span', 'É necessário informar o número do telefone principal.')
+    .should('be.visible')
+
+    cy.contains('span', 'É necessário informar um e-mail.')
+    .should('be.visible')
+    
+    cy.contains('span', 'Por favor, informe o nome e o sobrenome.')
+    .should('be.visible')
+  })
+
+  it('Faz cadastro com sucesso', () => {
+
+    cy.fixture("example.json").then((dados) => {
+
+      cy.get('input[name="name"]')
+      .type(dados.nomeCompleto)
+
+      cy.contains('span', 'Não informar')
+      .click()
+
+      cy.get('input[name="birthDate"]')
+      .type(dados.dataNascimento)
+
+      cy.get('input[name="cpf"]')
+      .type(dados.cpf)
+
+      cy.get('input[name="phone"]')
+      .type(dados.telefone)
+
+      cy.contains('span', 'Receber notificações por WhatsApp')
+      .click()
+
+      cy.get('input[name="cellphone"]')
+      .type(dados.celular)
+
+      cy.get('input[name="email"]')
+      .type(dados.email)
+
+      cy.get('input[name="password"]')
+      .type(dados.senha)
+
+      cy.get('span[class="sc-jvdzsS hXYyLP"]')
+      .should('have.text', 'ótima')
+
+      cy.intercept('https://www.google.com/recaptcha/api2/reload?k=6Lecgi4UAAAAADoy8olZEoD8T5dqynjjJajXSQHj')
+      .as('cadastroEfetuado')
+
+      cy.get('[data-cy="sign-submit"]')
+      .click()
+
+      cy.wait('@cadastroEfetuado')
+
+    });
+  })
+
+  it('Verifica mensagem de erro no cadastro duplicado', () => {
+      
+    cy.fixture("example.json").then((dadosDuplicado) => {
+
+      cy.get('input[name="name"]')
+      .type(dadosDuplicado.nomeCompleto)
+
+      cy.contains('span', 'Não informar')
+      .click()
+
+      cy.get('input[name="birthDate"]')
+      .type(dadosDuplicado.dataNascimento)
+
+      cy.get('input[name="cpf"]')
+      .type(dadosDuplicado.cpf)
+
+      cy.get('input[name="phone"]')
+      .type(dadosDuplicado.telefone)
+
+      cy.contains('span', 'Receber notificações por WhatsApp')
+      .click()
+      
+      cy.get('input[name="cellphone"]')
+      .type(dadosDuplicado.celular)
+
+      cy.get('input[name="email"]')
+      .type(dadosDuplicado.email)
+
+      cy.get('input[name="password"]')
+      .type(dadosDuplicado.senha)
+
+      cy.get('span[class="sc-jvdzsS hXYyLP"]')
+      .should('have.text', 'ótima')
+
+      cy.intercept('https://www.google.com/recaptcha/api2/reload?k=6Lecgi4UAAAAADoy8olZEoD8T5dqynjjJajXSQHj')
+      .as('cadastroEfetuado')
+
+      cy.get('[data-cy="sign-submit"]')
+      .click()
+
+      cy.wait('@cadastroEfetuado')
+    })
+
+  })
+})
